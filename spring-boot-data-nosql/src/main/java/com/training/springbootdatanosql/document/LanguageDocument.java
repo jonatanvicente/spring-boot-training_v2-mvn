8ca162789a48a79d21df9f659ce20308a95ad012
Jonatan Vicente <it@jonatanvicente.com>
2025-04-06 12:58:53 +0200

    Added module nosql

---
+++ spring-boot-data-nosql/src/main/java/com/training/springbootdatanosql/document/LanguageDocument.java
@@ -0,0 +1,23 @@
+package com.training.springbootdatanosql.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.UUID;
+
+@Document(collection="languages")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LanguageDocument {
+
+    @Id
+    @Field(name="id_languages")
+    private UUID idLanguage;
+
+    @Field(name="language_name")
+    private String languageName;
+
+}
